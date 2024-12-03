8c5d2900fc119a70ca59dcb65bcf4879fbb626eb
viveknextbill <vivek.yadav@nextbill.io>
2024-12-03 22:51:53 +0530

    Initialize express server

---
+++ index.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const port = process.env.PORT || 8000;
+
+const app = express();
+
+app.use("/", (req, res) => {
+  res.send("Health check");
+});
+
+app.listen(port, () => {
+  console.log("Running on port", port);
+});
